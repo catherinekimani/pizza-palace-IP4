912f4ba2aa7152ac7087fe9ffa39dcb41fd34872
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-03-19 23:06:29 +0300

    Add Jquery - to validate user input

--- Js/script.js
+++ Js/script.js
@@ -15,3 +15,57 @@ function send(){
         alert(name + " " + "We have received your message. Thank you for reaching out to us.");
     };
 }
+
+// Jquery form validate
+$(document).ready(function(){
+    $("#myPizza").checkout(function() {
+        // get user input
+        function pizza () {
+            var pizzaName = document.getElementById("pizza").value;
+            return parseInt(pizzaName);
+          }
+          function size() {
+            var pizzaSize = document.getElementById("size").value;
+            return parseInt(pizzaSize);
+          }
+          function crust() {
+            var pizzaCrust = document.getElementById("crust").value;
+            return parseInt(pizzaCrust);
+          }
+          function toppings() {
+            var pizzaToppings = document.getElementById("toppings").value;
+            return parseInt(pizzaToppings);
+          }
+          function number() {
+            var pizzaNumber = document.getElementById("pizzas").value;
+            return parseInt(pizzaNumber);
+          }
+
+        //   constructors
+        function Order(flavor, size, crust, topping, quantity) {
+            this.newPizza = pizza;
+            this.newSize = size;
+            this.newCrust = crust;
+            this.newToppings = toppings;
+            this.newPizzas = pizzas;
+          }
+
+
+          var inputs = new Order(pizza(), size(), crust(), toppings(), pizzas());
+
+          var totalAmount =
+      (inputs.newPizza +
+        inputs.newSize +
+        inputs.newCrust +
+        inputs.newToppings) *
+      inputs.newPizzas
+
+      alert("Your Charges are " + totalAmount);
+    prompt("Your Email Address");
+    prompt("enter your phone number");
+    prompt("Your Location");
+    alert("Your pizza will be delivered. Thanks for Reaching Out to Us");
+
+    $("#myPizza").reset();
+    });
+});
